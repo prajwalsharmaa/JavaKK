686d6f952e8973eecd86a7ce3e4f2ee8d2ae89b2
prajwalsharmaa <prajwalst1213@gmail.com>
2026-09-21 21:24:52 +0545

    linear search algo

--- linear-search-algo/src/Main.java
+++ linear-search-algo/src/Main.java
@@ -1,5 +1,17 @@
 public class Main{
     static void main() {
+        int[] arr = {2,3,5,6,3};
+        System.out.println(search(7,arr));
+    }
+    static String search(int x,int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == x){
+                return "Element found at index " + i;
+            }
+
+        }
+        return "Element not found in array";
 
     }
 }
