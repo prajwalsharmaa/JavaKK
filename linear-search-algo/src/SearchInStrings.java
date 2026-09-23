1662870931ceb21223863a7fcfbd111981acc8b5
prajwalsharmaa <prajwalst1213@gmail.com>
2026-09-23 09:27:55 +0545

    search char is string

--- linear-search-algo/src/SearchInStrings.java
+++ linear-search-algo/src/SearchInStrings.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class SearchInStrings {
     static void main() {
         String name = "Prajwal Sharma";
         char target = 'w';
-        System.out.println(search(name,target));
+        System.out.println(search2(name,target));
+        System.out.println(Arrays.toString(name.toCharArray()));
     }
-    static boolean search(String str, char target){
+    static boolean search1(String str, char target){
         if(str.length()== 0){
             return false;
         }
@@ -15,4 +18,27 @@ public class SearchInStrings {
         }
         return  false;
     }
+    static boolean search2(String str, char target){
+        if(str.length()== 0){
+            return false;
+        }
+        for(char ch : str.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return  false;
+    }
+    static boolean search3(String str, char target){
+        if(str.length()== 0){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return  false;
+    }
+
 }
